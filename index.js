06b284e45ffe095a57b07e6b9fba7694c6cdc609
Quenginedev <quenginedev@gmail.com>
2022-10-29 20:23:03 +0000

    Logging path issue

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 import { setFailed } from '@actions/core'
 import runner from "./lib/runner.js";
-import {compose, prop} from 'ramda'
-
-const setFailedMessage = compose(setFailed, prop('message')) 
-runner().catch(setFailedMessage);
+runner().catch(setFailed);
